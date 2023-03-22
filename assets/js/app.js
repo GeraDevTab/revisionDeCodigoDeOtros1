ab10e316a2841a9cc8ad2dab3255754ec48e2817
geradevtab <everythingissofast@gmail.com>
2023-03-22 13:08:58 -0600

    correciones al archivo js y al inde.html para mostrar informacion obtenida de una api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,47 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+
+//se cambio simbolo de gato(#) por un .(punto)
+const n = document.querySelector('.name');
+
+//se cambio simbolo de gato(#) por un .(punto)
+const b = document.querySelector('.blog');
+
+//se creo otro elemento de tipo p, para poder apuntarlo con el queryselector siguiente
+const l = document.querySelector('.location');
 
 function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //envio un console para comprobar si entra a esta funcion
+  console.log("entro a display");
+  n.textContent = 'cargando...';
+  //const response = await fetch(`${usersEndpoint}/${username}`);
+  const data =  fetch(`${usersEndpoint}/${username}`).
+  then(info=> info.json())
+  .then((data)=>{posicionar(data)});
+  //console.log(for(let i in data){console.log(i);}));
+  
+  
+}
+
+function posicionar(datos){
+  console.log("entro a posicionar");
+//n.textContent = '${data.name}';
+n.textContent = datos.login;
+
+//b.textContent = data.blog;
+b.textContent = datos.html_url;
+
+console.log(datos.blog);
+l.textContent =datos.organizations_url;
 }
 
 function handleError(err) {
+  //envio un console para comprobar si entra a esta funcion
+  console.log("entro a error");
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.innerHTML = `Algo salió mal: ${err}`
 }
-
-displayUser('stolinski').catch(handleError);
+//'tolinski'
+displayUser('tolinski');
+//.catch(handleError);
